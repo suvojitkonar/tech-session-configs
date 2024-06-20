5ed7ade606850e65387e340828247fa0d7d584dd
suvojitkonar <33071323+suvojitkonar@users.noreply.github.com>
2024-06-20 10:06:39 +0530

    Create question_1.py

---
+++ question_1.py
@@ -0,0 +1,8 @@
+def student_details(name, roll_number, marks):
+    print(f'Student name: {name}\nStudent roll number: {roll_number}\nStudent marks: {marks}')
+
+name = input('Enter student name: ')
+roll_number = input('Enter student roll number: ')
+marks = input('Enter student marks: ')
+
+student_details(name, roll_number, marks)
